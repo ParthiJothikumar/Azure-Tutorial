29459f02d41263c6efad298a391a7f5063bc0efc
Parthiban <parthijothikumar1998@gmail.com>
2025-07-30 09:03:22 +0530

    Debug true 9

--- azure_django/azure_django/wsgi.py
+++ azure_django/azure_django/wsgi.py
@@ -9,10 +9,13 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 
+import sys
+
 from django.core.wsgi import get_wsgi_application
 
 settings_module = 'azure_django.settings.production'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+print("✅ WSGI loaded with production settings", file=sys.stderr)
 
 application = get_wsgi_application()
