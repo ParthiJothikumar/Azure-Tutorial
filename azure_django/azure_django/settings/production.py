07675a1348b93e2da17c69951ef4d817d1b52a55
Parthiban <parthijothikumar1998@gmail.com>
2025-07-30 08:51:50 +0530

    Debug true 7

--- azure_django/azure_django/settings/production.py
+++ azure_django/azure_django/settings/production.py
@@ -11,18 +11,18 @@ DEBUG = False
 
 
 ALLOWED_HOSTS = [
-    os.environ.get('WEBSITE_HOSTNAME', 'localhost'), 
+    os.environ['WEBSITE_HOSTNAME'], 
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://'+os.environ.get('WEBSITE_HOSTNAME', 'localhost'),
+    'https://'+os.environ['WEBSITE_HOSTNAME'],
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'https://white-glacier-048b80e0f.1.azurestaticapps.net'
 ]
 
-SECRET_KEY = os.environ.get('MY_SECRET_KEY', '')
+SECRET_KEY = os.environ['MY_SECRET_KEY']
 
 STORAGES = {
     "default": {
